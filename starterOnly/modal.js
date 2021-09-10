bf06abe175fa0097ef078696925de5448252fa79
Charles Cailleteau <charles.cailleteau@protonmail.com>
2021-09-10 13:08:24 +0200

    fermeture modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,8 +9,10 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalcontent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn=document.querySelector(".bground .close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//close modal form
+modalCloseBtn.addEventListener("click",()=>{
+  modalcontent.style.animationName="modalclose";
+  modalcontent.style.animationFillMode="forward";
+  setTimeout(() => {
+    modalbg.style.display="none";
+    modalcontent.style.animationName="modalopen";
+  }, 800);
+})
 
 
