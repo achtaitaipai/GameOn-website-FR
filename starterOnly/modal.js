cf0bb41b5e8d7408cc7ac76e169ac832e0ae54ec
Charles Cailleteau <charles.cailleteau@protonmail.com>
2021-09-10 14:40:22 +0200

    correction ouverture/fermeture modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,24 +12,26 @@ const modalbg = document.querySelector(".bground");
 const modalcontent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalCloseBtn=document.querySelector(".bground .close")
+const modalCloseBtn=document.querySelector(".bground .close");
+// const submitBtn=document.querySelector()
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//launch modal form
+modalBtn.forEach((btn) => btn.addEventListener("click", ()=>{
+  // modalbg.style.display = "block";
+  console.log("oe")
+  modalbg.classList.add("visible");
+}));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
 
 //close modal form
 modalCloseBtn.addEventListener("click",()=>{
-  modalcontent.style.animationName="modalclose";
-  modalcontent.style.animationFillMode="forward";
-  setTimeout(() => {
-    modalbg.style.display="none";
-    modalcontent.style.animationName="modalopen";
-  }, 800);
+  // modalcontent.style.animationName="modalclose";
+  // modalcontent.style.animationFillMode="forward";
+  // setTimeout(() => {
+  //   modalbg.style.display="none";
+  //   modalcontent.style.animationName="modalopen";
+  // }, 750);
+  modalbg.classList.remove("visible");
 })
 
 
